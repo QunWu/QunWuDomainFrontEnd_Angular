de6ad57edc7987e053280a917f14d5d7eb2ec46f
QunWu <wuqun597090371@hotmail.com>
2017-07-10 00:15:44 -0700

    header version_2 Bug:burgerNaBar

--- src/app/shared/navigation-header/navigation-header.component.ts
+++ src/app/shared/navigation-header/navigation-header.component.ts
@@ -1,16 +1,26 @@
-import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-navigation-header',
   templateUrl: './navigation-header.component.html',
   styleUrls: ['./navigation-header.component.css']
 })
-export class NavigationHeaderComponent {
-  @ViewChild("navbar") navbar :ElementRef;
+export class NavigationHeaderComponent implements OnInit{
+  @ViewChild("navbar") navbar: ElementRef;
 
-  isNavBarResponsive : boolean= false;
+  isNavBarResponsive : boolean= true;
   constructor(private renderer:Renderer2) { }
-
+  ngOnInit(): void {
+    // console.log($(window).height());
+    $(window).scroll(function (event) {
+      // var scroll = $(window).scrollTop();
+      if($(window).scrollTop()>=$(window).height()/2){
+        $("#header").css({backgroundColor: 'black',opacity:0.8});
+      }else
+        $("#header").css({backgroundColor:'transparent',opacity:1});
+    });
+  }
   clickBurgerNav(){
     if(this.isNavBarResponsive){
       this.renderer.addClass(this.navbar.nativeElement,"responsive");
