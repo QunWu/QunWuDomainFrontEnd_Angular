254dc2b76d9d45cfc3cc5f2f5c175c16154b0fd8
QunWu <wuqun597090371@hotmail.com>
2017-07-14 00:54:06 -0700

    contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule} from '@angular/router';
@@ -47,6 +47,7 @@ const appRoutes : Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes)
   ],
--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, Validators, FormControl } from "@angular/forms";
+/**
+ * Google API key: AIzaSyDDvWXfZ2UwwbBr5Ga1yyaveMwtnDBa_d8
+ */
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  f: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.f = new FormGroup({
+      'content' : new FormControl(null, [Validators.required]),
+      'subject' : new FormControl(null, [Validators.required]),
+      'email' : new FormControl(null, [Validators.required, Validators.email])
+    });
+  }
+
+  onSubmit(){
+    console.log(this.f);
   }
 
 }
